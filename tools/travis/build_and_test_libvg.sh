170b12a98b7f473e8ac60141e7d3114be53790f2
Damian <damian@autistici.org>
2018-07-09 13:38:29 +0200

    hatred

--- tools/travis/build_and_test_libvg.sh
+++ tools/travis/build_and_test_libvg.sh
@@ -2,4 +2,5 @@
 
 cd libvg
 # --jobs 1 to prevent threading problems with the BYOND crate.
+curl https://sh.rustup.rs -sSf | sh -s --no-prompt
 RUST_BACKTRACE="1" RUST_TEST_THREADS=1 cargo test --jobs 1 --verbose
