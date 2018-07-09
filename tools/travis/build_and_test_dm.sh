65e9edaf6aab5f48d4a8fa7745c2b6cd70d68b68
Damian <damian@autistici.org>
2018-07-09 13:00:47 +0200

    maybe

--- tools/travis/build_and_test_dm.sh
+++ tools/travis/build_and_test_dm.sh
@@ -3,4 +3,4 @@
 source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
 DM_UNIT_TESTS="1" python tools/travis/build.py
 cp tools/travis/config/config.txt config/
-python tools/travis/run_dm_tests.py
+tools/travis/run_dm_tests.py
